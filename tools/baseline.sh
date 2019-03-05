3f16d8982c21a1003c8ded2bf38c19000cc8ac3e
yongbiaoai <yongbiaoai@gmail.com>
2019-03-05 19:39:17 +0800

    modify baseline.sh
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- tools/baseline.sh
+++ tools/baseline.sh
@@ -1,17 +1,17 @@
 
 
 gdown https://drive.google.com/uc?id=0ByhzM2YklhADNmk4cEN2MTA5U0E
-cp merged_frozen_graph.pb ../android/Application/src/main/assets/merged_frozen_graph.pb
+mv merged_frozen_graph.pb ../android/Application/src/main/assets/merged_frozen_graph.pb
 
 wget -c https://deepdetect.com/models/tf/inception_v4.pb
-cp inception_v4.pb ../tensorflow/ConvNets
+mv inception_v4.pb ../tensorflow/ConvNets
 
 wget -c http://shannon.cs.illinois.edu/DenotationGraph/data/flickr30k-images.tar
 tar -zxvf flickr30k-images.tar ../tensorflow/Dataset/flickr30k-images
 
 wget -c http://shannon.cs.illinois.edu/DenotationGraph/data/flickr30k.tar.gz
 tar -zxvf flickr30k.tar.gz ./
-cp ./flickr30k/results_20130124.token ../tensorflow/Dataset/flickr30k-images
+mv ./flickr30k/results_20130124.token ../tensorflow/Dataset/flickr30k-images
 rm -rf ./flickr30k
 
 python convfeatures.py --data_path ../tensorflow/Dataset/flickr30k-images --inception_path ../tensorflow/ConvNets/inception_v4.pb
