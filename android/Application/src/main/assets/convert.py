9a871da96035047c996942ff0569edd91c21f2a2
yongbiaoai <yongbiaoai@gmail.com>
2019-04-14 12:32:03 +0800

    delete non-sense files
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- android/Application/src/main/assets/convert.py
+++ android/Application/src/main/assets/convert.py
@@ -7,6 +7,6 @@ wtoidx = sorted(wtoidx.items(), key=operator.itemgetter(1))
 
 wtoidx = [i[0] for i in wtoidx]
 
-with open('wtoidx.txt', 'w') as f:
+with open('idmap', 'w') as f:
     for item in wtoidx:
         f.write("%s\n" % item)
